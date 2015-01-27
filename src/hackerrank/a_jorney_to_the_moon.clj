b7ef2a6275a9c620cbac658a07e19ae6debb2cc8
Odomontois <odomontois@gmail.com>
2015-01-27 17:29:37 +0300

    usuccessfull algo

--- src/hackerrank/a_jorney_to_the_moon.clj
+++ src/hackerrank/a_jorney_to_the_moon.clj
@@ -1,34 +1,36 @@
-(ns hackerrank.a-jorney-to-the-moon (:require [clojure.string :refer [split]]))
+(ns hackerrank.a-jorney-to-the-moon
+  (:require [clojure.string :refer [split]]))
 (defn combinations [s k]
   (case k
     0 []
     1 (map list s)
     (if (empty? s) s
                    (lazy-cat
-                     (for [u (combinations (rest s) (dec k))] (cons (first s) u) )
+                     (for [u (combinations (rest s) (dec k))] (cons (first s) u))
                      (combinations (rest s) k)))))
-(defn init-sets [n] (vec (repeat n 1)))
+(defn init-sets [n]  (vec (repeat n 1)))
 (defrecord Set [root count path])
 (defn get-set [sets i]
   (let [v (sets i)]
-    (if (pos? v) (->Set i v [])
-      (let [s (get-set sets (- v))]
-        (update-in s [:path] conj i)))))
+    (if (pos? v) i
+                 (let [s (get-set sets (- v))]
+                   (assoc! sets i (- s))
+                   s))))
 (defn merge-sets
-  ([sets i j]
-  (let [si         (get-set sets i)
-        sj         (get-set sets j)
-        [prim sec] (if (> (:count si) (:count sj))
-                     [si sj] [sj si])
-        upd-idx    (conj (concat (:path si) (:path sj)) (:root sec))
-        root       (:root prim)
-        count      (+ (:count si) (:count sj))]
-    (apply assoc sets root count (interleave upd-idx (repeat (- root))))))
-  ([sets [i j]] (merge-sets sets i j)))
+  ([sets ^long i ^long j]
+    (let [si (get-set sets i)
+          sj (get-set sets j)
+          sa (+ (sets si) (sets sj))]
+      (if (> (sets si) (sets sj))
+        (assoc! sets sj (- si) si sa)
+        (assoc! sets si (- sj) sj sa)))))
 (defn variants [sets] (reduce + (for [[i j] (combinations (filter pos? sets) 2)] (* i j))))
 (defn read-ints [] (map #(Long/parseLong %) (split (read-line) #"\s+")))
-(defn solution [n merges] (variants (reduce merge-sets (init-sets n) merges)))
+(defn solution [n merges]
+  (let [sets (transient (init-sets n))]
+    (doseq [[i j] merges] (merge-sets sets i j))
+    (variants (persistent! sets))))
 (defn main []
-  (let [[n k]  (read-ints)
+  (let [[n k] (read-ints)
         merges (repeatedly k read-ints)]
     (println (solution n merges))))
