426c2b2799f78d2c2e578b253043f3d61f473960
Odomontois <odomontois@gmail.com>
2015-01-28 11:37:29 +0300

    usuccessfull algo

--- src/hackerrank/a_jorney_to_the_moon.clj
+++ src/hackerrank/a_jorney_to_the_moon.clj
@@ -4,35 +4,19 @@
 (defn get-set [sets i]
   (let [v (sets i)]
     (if (pos? v)
-      i
-      (let [s (get-set sets (- v))]
-        (assoc! sets i (- s))
-        s))))
-(defn get-set' [sets i]
-  (let [v (sets i)]
-    (if (pos? v)
       [sets i]
-      (let [[sets' s] (get-set' sets (- v))]
+      (let [[sets' s] (get-set sets (- v))]
         [(assoc sets' i (- s)) s]))))
-(defn merge-sets'
+(defn merge-sets
   ([sets [^long i ^long j]]
-    (let [[sets' si] (get-set' sets i)
-          [sets'' sj] (get-set' sets' j)
+    (let [[sets' si] (get-set sets i)
+          [sets'' sj] (get-set sets' j)
           sa (+ (sets si) (sets sj))]
       (cond
         (= si sj) sets
         (> (sets si) (sets sj)) (assoc sets'' sj (- si) si sa)
         :else (assoc sets'' si (- sj) sj sa)))))
-(defn merge-all' [n merges] (reduce merge-sets' (init-sets n) merges))
-(defn merge-sets
-  ([sets ^long i ^long j]
-    (let [si (get-set sets i)
-          sj (get-set sets j)
-          sa (+ (sets si) (sets sj))]
-      (when (not= si sj)
-        (if (> (sets si) (sets sj))
-          (assoc! sets sj (- si) si sa)
-          (assoc! sets si (- sj) sj sa))))))
+(defn merge-all [n merges] (reduce merge-sets (init-sets n) merges))
 (defn square [x] (* x x))
 (defn variants [sets]
   (let [counts (filter pos? sets)
@@ -41,10 +25,7 @@
         ssum (reduce + squares)]
     (quot (- (square csum) ssum) 2)))
 (defn read-ints [] (map #(Long/parseLong %) (split (read-line) #"\s+")))
-(defn solution [n merges]
-  (let [sets (transient (init-sets n))]
-    (doseq [[i j] merges] (merge-sets sets i j))
-    (variants (persistent! sets))))
+(defn solution [n merges] (variants (merge-all n merges)))
 (defn main []
   (let [[n k] (read-ints)
         merges (repeatedly k read-ints)]
