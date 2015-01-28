cb13fbfc36b2b4ab6837c98ff61c29e386d17128
Odomontois <odomontois@gmail.com>
2015-01-28 11:33:23 +0300

    usuccessfull algo

--- src/utils/readers.clj
+++ src/utils/readers.clj
@@ -1,6 +1,11 @@
 (ns ^{:author Odomontois} utils.readers
-  (:require [clojure.string :refer [split]])
-  (:import  [java.lang.BigInteger]))
-(defn read-int [] (Long/parseLong (read-line)))
-(defn read-ints [] (map #(Long/parseLong %) (split (read-line) #"\s+")))
-(defn read-bin [](BigInteger. (read-line) 2))
+  (:require [clojure.string :refer [split]]
+            [clojure.java.io :as jio]
+            [clojure-csv.core :refer [parse-csv]])
+  (:import [java.lang.BigInteger]))
+(defn ^Long parse-int [^String s] (Long/parseLong s))
+(defn read-int [] (parse-int (read-line)))
+(defn read-ints [] (map parse-int (split (read-line) #"\s+")))
+(defn read-bin [] (BigInteger. (read-line) 2))
+(defn slurp-resource [name] (slurp (-> name jio/resource jio/file)))
+(defn read-csv [name f & options] (map f (apply parse-csv (slurp-resource name) options)))
