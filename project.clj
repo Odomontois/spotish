bc37b51baa22abb214fe5d77feccb582ae841280
Odomontois <odomontois@gmail.com>
2015-01-30 09:10:25 +0300

    Lumump

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject spotish "INITIAL" :dependencies [[org.clojure/clojure "1.6.0"]])
+(defproject
+  spotish "INITIAL"
+  :dependencies
+  [[org.clojure/clojure "1.6.0"]
+   [clojure-csv/clojure-csv "2.0.1"]
+   [environ "1.0.0"]])
