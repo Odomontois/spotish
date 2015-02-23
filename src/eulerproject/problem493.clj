16cebf6d39c53be38440b19e655091803ddb1ff2
Odomontois <odomontois@gmail.com>
2015-02-23 21:50:50 +0300

    euler 493

--- src/eulerproject/problem493.clj
+++ src/eulerproject/problem493.clj
@@ -14,6 +14,25 @@
                r (shares f (dec k) (- n f))]
            (cons f r))))))
   ([k n] (shares n k n)))
+(defn sum [c] (reduce + 0 c))
+(defn prod [c] (reduce * 1 c))
+(defn groups [s k n] (map frequencies (shares s k n)))
+(defn pow [x p] (prod (repeat p x)))
+(defn grc [m s r] (pow (C s m) r))
 
-(def u (map frequencies (shares 10 7 20)))
+(defn vars [s k n]
+  (for [g (groups s k n)
+        :let [grcs (for [[m r] g] (grc m s r))
+              col (map second g)
+              colcnt (sum col)
+              cc (/ (! k) (! (- k colcnt)) (prod (map ! col)))
+              ]]
+    [colcnt (* (prod grcs) cc)]))
+
+(defn bycnt [s k n] (for [[c cnts] (group-by first (vars s k n))] [c (sum (map second cnts))]))
+(defn result [s k n] ( / (sum (for [[k c] (bycnt s k n)] (* k c))) (C (* s k) n)))
+
+;(def result (sum (vars 10 7 20)))
+
+
 
